309dd72d592b47428fe0d7e3c019d4e1b340ef07
menta <menta@DESKTOP-9R8BU89>
2016-04-13 08:40:25 +0200

    Add project files.

---
+++ WeatherDemo/gruntfile.js
@@ -0,0 +1,23 @@
+﻿/// <binding BeforeBuild='default' />
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                     { src: 'node_modules/jquery/dist/jquery.min.js', dest: 'www/scripts/lib/jquery.min.js' },
+                     { src: 'node_modules/jquery/dist/jquery.min.js.map', dest: 'www/scripts/lib/jquery.min.js.map' },
+                     { src: 'node_modules/jquery/dist/jquery.js', dest: 'www/scripts/lib/jquery.js' },
+                     { src: 'node_modules/jquery-mobile/dist/jquery.mobile.js', dest: 'www/scripts/lib/jquery.mobile.js' },
+                     { src: 'node_modules/jquery-mobile/dist/*.css', dest: 'www/css/*.css' }
+                ]
+
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.registerTask('default', ['copy']);
+};
---
+++ WeatherDemo/www/scripts/weather.js
@@ -0,0 +1,47 @@
+﻿var WeatherApp = {};
+
+(function ($, ns, navigator) {
+    ns.getWeather = function () {
+        var zipcode = $('#zip-code-input').val();
+
+        // get weather using zip code
+        var queryString =
+            'http://api.openweathermap.org/data/2.5/weather?q='
+            + zipcode + '&units=metric&APPID=ce6a2c4ede27ce584cfbd36373ca1675';
+
+        $.getJSON(queryString, function (results) {
+            if (results) {
+                $('#error-msg').hide();
+                $('#weather-data').show();
+
+                var weather = results;
+                $('#title').text(weather.name);
+
+                var wind = weather.wind;
+                $('#temperature').text(weather.main.temp);
+                $('#wind').text(wind.speed);
+
+                //var atmosphere = weather.atmosphere;
+                //$('#humidity').text(atmosphere.humidity);
+                //$('#visibility').text(atmosphere.visibility);
+
+                //var astronomy = weather.astronomy;
+                //$('#sunrise').text(astronomy.sunrise);
+                //$('#sunset').text(astronomy.sunset);
+
+                $('#summary img').attr('src', 'http://openweathermap.org/img/w/'+weather.weather[0].icon+'.png');
+
+            } else {
+                $('#weather-data').hide();
+                $('#error-msg').show();
+                $('#error-msg').text("Error retrieving data. " + results.query.results.channel.item.title);
+            }
+        }).fail(function (jqXHR) {
+            $('#error-msg').show();
+            $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+        });
+
+        return false;
+    }
+
+})($, WeatherApp, navigator);
